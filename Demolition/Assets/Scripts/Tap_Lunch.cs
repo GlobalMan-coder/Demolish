55428f67fc7c762960895c0d80e9f5201f713e2c
GlobalMan-coder <68968348+GlobalMan-coder@users.noreply.github.com>
2020-09-30 06:25:16 +0800

    tap & lunck fixing

--- Demolition/Assets/Scripts/Tap_Lunch.cs
+++ Demolition/Assets/Scripts/Tap_Lunch.cs
@@ -6,7 +6,7 @@ public class Tap_Lunch : MonoBehaviour
 {
     public float forwardForce;
     public float upForce;
-    public Transform canvasTransform;
+    public Transform targetParent;
     public float divisionDeviationX;
     public float divisionDeviationY;
     public float duration = 3f;
@@ -14,45 +14,54 @@ public class Tap_Lunch : MonoBehaviour
     public GameObject trackPrefab;
     public GameObject camera;
 
+    List<Vector3> dirs;
     Vector3 startingPositionOfThrow;
+
     void Start()
     {
-        startingPositionOfThrow = new Vector3(camera.transform.position.x, camera.transform.position.y - 2f, camera.transform.position.z);
+        dirs = new List<Vector3>();
     }
-
     // Update is called once per frame
-    void Update()
+    public void Touch()
     {
-        if (Input.GetMouseButtonDown(0))
+        Vector3 mousePos = Input.mousePosition;
+        startingPositionOfThrow = new Vector3(camera.transform.position.x, camera.transform.position.y, camera.transform.position.z);
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        Vector3 dir;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Vector3 dir;
-            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
-            {
-                dir = (hit.point - startingPositionOfThrow).normalized;
-            }
-            else
-            {
-                dir = (ray.direction * 1000 + camera.transform.position - startingPositionOfThrow).normalized;
-            }
-            ThrowBox(dir);
+            dir = (hit.point - startingPositionOfThrow).normalized;
         }
+        else
+        {
+            dir = (ray.direction * 1000 + camera.transform.position - startingPositionOfThrow).normalized;
+        }
+        GameObject track = Instantiate(trackPrefab, targetParent);
+        track.transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+        dirs.Add(dir);
     }
-
-    void ThrowBox(Vector3 dir)
+    public void ThrowBoxes()
+    {
+        for(int i = targetParent.childCount - 1; i >=0 ; i--)
+        {
+            Destroy(targetParent.GetChild(i).gameObject);
+        }
+        for(int i = 0; i < dirs.Count; i++)
+        {
+            ThrowBox(dirs[i]);
+        }
+        dirs.Clear();
+    }
+    Transform ThrowBox(Vector3 dir)
     {
         GameObject oneBox = Instantiate(boxPrefab, startingPositionOfThrow, camera.transform.rotation);
         oneBox.GetComponent<Rigidbody>().AddForce(dir * forwardForce, ForceMode.Impulse);
-
         float randomXtorque = Random.Range(-8f, 8f);
         float randomYTorque = Random.Range(-8f, 8f);
         float randomZTorque = Random.Range(-8f, 8f);
-
         oneBox.GetComponent<Rigidbody>().AddTorque(new Vector3(randomXtorque, randomYTorque, randomZTorque), ForceMode.Impulse);
-        GameObject track = Instantiate(trackPrefab, canvasTransform);
-        track.GetComponent<LunchSprite>().bullet = oneBox.transform;
         Destroy(oneBox, duration);
-        Destroy(track, duration);
+        return oneBox.transform;
     }
 }
