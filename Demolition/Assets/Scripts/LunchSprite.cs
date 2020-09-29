7ecb08ce6bc8ddd77df41feb64fe1a7a01b5c0ed
GlobalMan-coder <68968348+GlobalMan-coder@users.noreply.github.com>
2020-09-29 08:01:26 +0800

    tap & lunch

---
+++ Demolition/Assets/Scripts/LunchSprite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LunchSprite : MonoBehaviour
+{
+    [HideInInspector] public Transform bullet;
+    //float firstDistance = 2f;
+    //float deviation;
+    float scale = 1f;
+    Camera camera;
+    void Start()
+    {
+        camera = Camera.main;
+        //deviation = firstDistance - (camera.transform.position - bullet.position).magnitude;
+    }
+    void Update()
+    {
+        transform.position = camera.WorldToScreenPoint(bullet.position);
+        //transform.localScale = Vector3.one * firstDistance / ((camera.transform.position - bullet.position).magnitude + deviation);
+        if(scale > 0){
+            scale *= 0.9f;
+        }
+        transform.localScale = Vector3.one * scale;
+    }
+}
---
+++ Demolition/Assets/Scripts/Tap_Lunch.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap_Lunch : MonoBehaviour
+{
+    public float forwardForce;
+    public float upForce;
+    public Transform canvasTransform;
+    public float divisionDeviationX;
+    public float divisionDeviationY;
+    public float duration = 3f;
+    public GameObject boxPrefab;
+    public GameObject trackPrefab;
+    public GameObject camera;
+
+    Vector3 startingPositionOfThrow;
+    void Start()
+    {
+        startingPositionOfThrow = new Vector3(camera.transform.position.x, camera.transform.position.y - 2f, camera.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 dir;
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                dir = (hit.point - startingPositionOfThrow).normalized;
+            }
+            else
+            {
+                dir = (ray.direction * 1000 + camera.transform.position - startingPositionOfThrow).normalized;
+            }
+            ThrowBox(dir);
+        }
+    }
+
+    void ThrowBox(Vector3 dir)
+    {
+        GameObject oneBox = Instantiate(boxPrefab, startingPositionOfThrow, camera.transform.rotation);
+        oneBox.GetComponent<Rigidbody>().AddForce(dir * forwardForce, ForceMode.Impulse);
+
+        float randomXtorque = Random.Range(-8f, 8f);
+        float randomYTorque = Random.Range(-8f, 8f);
+        float randomZTorque = Random.Range(-8f, 8f);
+
+        oneBox.GetComponent<Rigidbody>().AddTorque(new Vector3(randomXtorque, randomYTorque, randomZTorque), ForceMode.Impulse);
+        GameObject track = Instantiate(trackPrefab, canvasTransform);
+        track.GetComponent<LunchSprite>().bullet = oneBox.transform;
+        Destroy(oneBox, duration);
+        Destroy(track, duration);
+    }
+}
